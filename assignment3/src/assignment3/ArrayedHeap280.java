8edc6e101f94c3f318c3fb4e98c4524617a10d2d
Carter Hill <carterghill@gmail.com>
2016-02-01 20:18:25 -0600

    Testing completed on ArrayedHeap280

--- assignment3/src/assignment3/ArrayedHeap280.java
+++ assignment3/src/assignment3/ArrayedHeap280.java
@@ -39,15 +39,20 @@ public class ArrayedHeap280<I extends Comparable<? super I>> extends
 		int prev = this.currentNode;
 		
 		if(this.currentNode > 1){
-			//prev = this.currentNode;
 			this.currentNode = this.findParent(currentNode);
 		}
 		
 		while (x.compareTo(this.item()) > 0
-				&& this.currentNode > 1) {
-			I[] temp = items.clone();
-			this.items[currentNode] = temp[prev];
-			this.items[prev] = temp[currentNode];
+				&& this.currentNode > 0) {
+
+			I y = this.items[currentNode];
+			this.items[currentNode] = x;
+			this.items[prev] = y;
+
+			prev = this.currentNode;
+			
+			if(this.currentNode > 1)
+				this.currentNode = this.findParent(currentNode);
 		}
 
 	}
@@ -108,12 +113,22 @@ public class ArrayedHeap280<I extends Comparable<? super I>> extends
 	private I viewTop(){
 		return this.items[1];
 	}
+	
+	private void viewHeap(){
+		System.out.println("Here is your heap:\n==================");
+		for(int i = 1; i < this.count+1; i++){
+			this.currentNode = i;
+			System.out.println(this.item());
+		}
+		System.out.println("==================");
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		
+		Boolean success = true;
 		ArrayedHeap280<Integer> H = new ArrayedHeap280<Integer>(10);
 
 		// IsEmpty on empty tree.
@@ -131,20 +146,82 @@ public class ArrayedHeap280<I extends Comparable<? super I>> extends
 				System.out.println("Expected exception deleting item in empty heap.  Got none.");
 		}
 		
-		//Only item is four, check if in the heap
+		// Only item is four, check if in the heap
 		H.insert(4);
 		if(!(H.viewTop()==4))
 			System.out.println("Test on insert(4) failed");
 		
-		//After inserting 3, four should stay on top
+		// After inserting 3, four should stay on top
 		H.insert(3);
 		if(!(H.viewTop()==4))
 			System.out.println("Test on insert(3) failed");
 		
 		H.insert(2);
+		// Delete should remove the highest number and replace it with three
 		H.deleteItem();
 		if(!(H.viewTop()==3))
 			System.out.println("Test on deleteItem() failed");
+		H.deleteItem();
+		// Top should now be two
+		if(!(H.viewTop()==2))
+			System.out.println("Test on deleteItem() failed");
+		
+		// Overload heap and catch exception
+		H.insert(4);
+		H.insert(5);
+		H.insert(10);
+		H.insert(1);
+		H.insert(88);
+		H.insert(-69);
+		H.insert(8);
+		H.insert(11);
+		H.insert(42);
+		
+		x = null;
+		try {
+			H.insert(0);
+		} catch (ContainerFull280Exception e) {
+			x = e;
+		} finally {
+			if (x == null){
+				System.out.println("Expected exception dinserting to many items.  Got none.");
+				success = false;
+			}
+		}
+		
+		// Make sure children are small than parents
+		for(int i = 1; i < H.count; i++){
+			H.currentNode = i;
+			int left = H.findLeftChild(i);
+			int right = H.findRightChild(i);
+			if(left <= H.count && right <= H.count){
+				int a = H.item();
+				H.currentNode = left;
+				int b = H.item();
+				H.currentNode = right;
+				int c = H.item();
+				if(a < b || a < c){
+					System.out.println("Parent smaller than child, error!");
+					success = false;
+				}
+			} else if(left <= H.count){
+				int a = H.item();
+				H.currentNode = left;
+				int b = H.item();
+				if(a < b){
+					System.out.println("Parent smaller than child, error!");
+					success = false;
+				}
+			}
+		}
+		
+		if(success) {
+			System.out.println("Testing Success!\n");
+			H.viewHeap();
+		} else {
+			System.out.println("Testing failed :(");
+		}
+			
 
 	}
 
